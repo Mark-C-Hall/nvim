0490fa5b3c78eb8f84be4007542838f41c148322
Mark Hall <mark.hall@avature.net>
2024-08-08 16:09:41 -0400

    Update mappings and options

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,3 +9,15 @@ map("i", "jk", "<ESC>")
 map("n", "<leader>mi", vim.cmd.MasonInstallAll)
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("v", "<A-c>", '"+y')
+
+-- Map Ctrl+d and Ctrl+u to scroll and center the cursor with zz
+map("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
+map("n", "<C-f>", "<C-f>zz", { desc = "Page up and center" })
+map("n", "<C-b>", "<C-b>zz", { desc = "Page up and center" })
+
+-- Remap Horizontal Term
+map({ "n", "t" }, "<A-x>", function()
+  require("nvchad.term").toggle { pos = "sp", id = "horizontalTerm" }
+end, { desc = "Terminal Toggle Horizontal term" })
